d70aa677e2243d857d0c4b34e82579744790cbb1
kx1t <kx1t@amsat.org>
2022-12-26 08:25:39 -0500

    updates kladblok

--- kladblok/getais.sh
+++ kladblok/getais.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #shellcheck shell=bash disable=SC2076
-AIS_URL="https://kx1t.com/aistships_full.json"
 
-vesseldata="$(jq -r ".ships[]|to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" < curl -Ls "$AIS_URL")"
+#set -x
+AIS_URL="https://kx1t.com/ais/ships_full.json"
+
+vesseldata="$(curl -sL "$AIS_URL" | jq -r ".ships[]|to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]")"
 
 declare -A VESSELS
 declare -a VESSEL_INDEX
